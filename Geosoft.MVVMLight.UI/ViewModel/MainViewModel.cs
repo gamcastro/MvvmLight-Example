6661f9b7083c3c21e5736300d8f592021d18b8d7
George Castro <gamcastro14@gmail.com>
2021-06-06 22:05:32 -0300

    Atualizado MainWindowViewModel.

--- Geosoft.MVVMLight.UI/ViewModel/MainViewModel.cs
+++ Geosoft.MVVMLight.UI/ViewModel/MainViewModel.cs
@@ -1,4 +1,9 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using Geosoft.MVVMLight.UI.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Geosoft.MVVMLight.UI.ViewModel
 {
@@ -19,16 +24,49 @@ namespace Geosoft.MVVMLight.UI.ViewModel
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
+        /// 
+        private ObservableCollection<Employee> employees;
+        private Employee selectedEmployee;
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            LoadEmployeesCommand = new RelayCommand(LoadEmployeesMethod);
+            SaveEmployeesCommand = new RelayCommand(SaveEmployeesMethod);
+        }
+
+        public ICommand LoadEmployeesCommand { get; private set; }
+        public ICommand SaveEmployeesCommand { get; private set; }
+
+        public ObservableCollection<Employee> EmployeeList
+        {
+            get
+            {
+                return employees;
+            }
+        }
+
+        public Employee SelectedEmployee
+        {
+            get
+            {
+                return selectedEmployee;
+            }
+            set
+            {
+                selectedEmployee = value;
+                RaisePropertyChanged("SelectedEmployee");
+            }
+        }
+
+        public void SaveEmployeesMethod()
+        {
+            Messenger.Default.Send<NotificationMessage>(new NotificationMessage("Employees Saved."));
+        }
+
+        private void LoadEmployeesMethod()
+        {
+            employees = Employee.GetSampleEmployees();
+            this.RaisePropertyChanged(() => this.EmployeeList);
+            Messenger.Default.Send<NotificationMessage>(new NotificationMessage("Employees Loaded."));
         }
     }
 }
